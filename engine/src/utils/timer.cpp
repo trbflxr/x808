d71041db052c10883450a883e4c33d824933602d
FLXR <igor.tsarev17@gmail.com>
2018-06-29 05:57:00 +0300

    timer and timestep

---
+++ engine/src/utils/timer.cpp
@@ -0,0 +1,43 @@
+//
+// Created by FLXR on 6/29/2018.
+//
+
+#include <new>
+#include <windows.h>
+
+#include "timer.hpp"
+
+namespace xe {
+
+	struct TimerMembers {
+		LARGE_INTEGER start;
+		double frequency;
+	};
+
+}
+
+xe::Timer::Timer() :
+		members(new(reserved) TimerMembers()) {
+
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+	members->frequency = 1.0f / frequency.QuadPart;
+
+	reset();
+}
+
+void xe::Timer::reset() {
+	QueryPerformanceCounter(&members->start);
+}
+
+float xe::Timer::elapsed() {
+	LARGE_INTEGER current;
+	QueryPerformanceCounter(&current);
+
+	LONGLONG cycles = current.QuadPart - members->start.QuadPart;
+	return static_cast<float>(cycles * members->frequency);
+}
+
+float xe::Timer::elapsedMillis() {
+	return elapsed() * 1000.0f;
+}
